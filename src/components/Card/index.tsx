07bd481ae6911edf5d55a8163dd0070525307b3f
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2021-09-27 22:43:01 -0300

    Pequenos refinamentos no Layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./style.scss";
 
 interface CardProps {
@@ -7,11 +8,22 @@ interface CardProps {
 }
 
 export const Card = ({data, titulo, text}: CardProps) => {
+  const [like, setLike] = useState(0);
+
+  const [icon, setIcon] = useState([
+    <i className="far fa-heart"></i>,
+    <i className="fas fa-heart"></i>
+  ]);
+
+  function alterLike(){
+    like ? setLike(0) : setLike(1);
+  }
+  
   return (
     <div className="card">
       <div className="card_data">
         <span>{data}</span>
-        <span><i className="far fa-heart"></i></span>
+        <span onClick={alterLike}>{icon[like]}</span>
       </div>
       <div className="card_detais">
         <h3>{titulo}</h3>
